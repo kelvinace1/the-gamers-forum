e37c4bbd8b76529a61543862102746ffe3495728
Kelvinace <salamisamod5@gmail.com>
2021-12-20 14:21:53 +0100

    Updated urls.py
    Mapping the class based Views to the urls.py

--- forums/urls.py
+++ forums/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-
+from .views import PostAdd, PostDeleteView
 
 urlpatterns = [
     path('', views.TopicList.as_view(), name='homepage'),
     path('topic/<str:name>/', views.TopicDetail.as_view(), name='topic_detail'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('add-post/', views.PostAdd.as_view(), name='add_post'),
+    path('add-post/', PostAdd.as_view(), name='add_post'),
     path('edit-post/<slug:slug>/', views.PostEdit.as_view(), name='edit_post'),
-    path('delete-post/<slug:slug>/', views.delete_post, name='delete_post'),
+    path('delete-post/<slug:slug>/', PostDeleteView, name='delete_post'),
 ]
